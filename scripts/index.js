e422617c5f3680598ceebc2775019ff9c77afa3a
iago-monteirog <iagomgarcia9000@gmail.com>
2020-03-18 21:30:57 -0300

    add the scroll slide function

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,23 @@ const sliderImages = document.querySelectorAll('.slide-in');
 
 function checkSlide(e) {
     //  console.log(e);
-    
+    sliderImages.forEach(sliderImage => {
+        // ate o meio da imagem
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // ate o "pé" da imagem
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+
+    });
 }
 
 window.addEventListener('scroll', debounce(checkSlide));
